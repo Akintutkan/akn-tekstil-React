dd3a6d04979b5deaaa503024392d02db13e71ff6
Akin94 <akin.tutkan@gmail.com>
2022-11-27 18:51:56 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+const App = () =>{
+  return <div className="kategoriler-konteynırı">
+    <div className="kategori-konteynırı">{/*<img src="" alt=""/>*/}
+<div className="kategori-gövde-konteynırı">
+  <h2>Hats</h2>
+  <p>Shop Now</p>
+</div>
+    </div>
+    <div className="kategori-konteynırı">{/*<img src="" alt=""/>*/}
+<div className="kategori-gövde-konteynırı">
+  <h2>Jackets</h2>
+  <p>Shop Now</p>
+</div>
+    </div>
+    <div className="kategori-konteynırı">{/*<img src="" alt=""/>*/}
+<div className="kategori-gövde-konteynırı">
+  <h2>Sneakers</h2>
+  <p>Shop Now</p>
+</div>
+    </div>
+    <div className="kategori-konteynırı">{/*<img src="" alt=""/>*/}
+<div className="kategori-gövde-konteynırı">
+  <h2>Mens</h2>
+  <p>Shop Now</p>
+</div>
+    </div>
+    <div className="kategori-konteynırı">{/*<img src="" alt=""/>*/}
+<div className="kategori-gövde-konteynırı">
+  <h2>Womens</h2>
+  <p>Shop Now</p>
+</div>
+    </div>
+  </div>
+}
+
+export default App;
