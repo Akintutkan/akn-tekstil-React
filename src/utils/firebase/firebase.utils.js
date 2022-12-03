db0acc94904001088c0a21e735b78da68e6ea121
Akin94 <akin.tutkan@gmail.com>
2022-12-03 19:44:06 +0300

    create Firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDjdk1DV68nPFqElvEaKM7GlWNQJpav8r4",
+    authDomain: "akn-tekstil.firebaseapp.com",
+    projectId: "akn-tekstil",
+    storageBucket: "akn-tekstil.appspot.com",
+    messagingSenderId: "1027812059667",
+    appId: "1:1027812059667:web:bf1d9da07403e7dbb9ede6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
