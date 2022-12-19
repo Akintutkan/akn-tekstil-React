89e44c5ba4e6f298aabadb002ad803b5c7d757f8
Akin94 <akin.tutkan@gmail.com>
2022-12-20 01:49:49 +0300

    Firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,7 +5,12 @@ import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
 import { Await } from "react-router-dom";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   //auth Code
+  apiKey: "AIzaSyDjdk1DV68nPFqElvEaKM7GlWNQJpav8r4",
+  authDomain: "akn-tekstil.firebaseapp.com",
+  projectId: "akn-tekstil",
+  storageBucket: "akn-tekstil.appspot.com",
+  messagingSenderId: "1027812059667",
+  appId: "1:1027812059667:web:bf1d9da07403e7dbb9ede6"
   };
   
   // Initialize Firebase
