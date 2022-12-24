e3397d954ba5a8d6e3fb2b1638e5f0fe845137be
Akin94 <akin.tutkan@gmail.com>
2022-12-24 23:39:21 +0300

    Add link Home item Nagivate to child component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,22 +1,23 @@
-
+import React from "react"
 import "./category-item.styles.scss"
-
+import { useNavigate } from "react-router-dom";
 
 
 
 const CategoryItem =({category}) => {
     const {imageUrl,title,linkUrl} = category
+    let navigate = useNavigate();
 return (
-<div className="kategori-konteynırı" 
-onClick={() => (`${linkUrl}`)}    
- >
-<div className="background-image"
-style={{backgroundImage:`url(${imageUrl})`}}>
+    
+<div className="kategori-konteynırı"  onClick={() => {navigate(`/${linkUrl}`);
+      }}>
+<div className="background-image"style={{backgroundImage:`url(${imageUrl})`}}>
 </div>
 <div className="kategori-gövde-konteynırı">
 <h2>{title}</h2>
 <p>Shop Now</p>
 </div>
-</div>)
+</div>
+)
 }
 export default CategoryItem
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,27 +7,31 @@ const Home = () =>{
         "id": 1,
         "title": "hats",
         "imageUrl": "https://i.ibb.co/cvpntL1/hats.png",
-         "linkUrl": "../../products/HatProduct"
+        "linkUrl" : "hat"
       },
       {
         "id": 2,
         "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png",
+        "linkUrl" : "jacket"
       },
       {
         "id": 3,
         "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png",
+        "linkUrl" : "sneaker"
       },
       {
         "id": 4,
         "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png",
+        "linkUrl" : "women"
       },
       {
         "id": 5,
         "title": "mens",
         "imageUrl": "https://i.ibb.co/R70vBrQ/men.png",
+        "linkUrl" : "men"
       }
     ]
   
@@ -35,6 +39,7 @@ const Home = () =>{
     <div>
         <Directory kategoriler={kategoriler}/>
          <Outlet/> {/*aşağı alırsam aşağıda render olur */}
+         
     </div>
   )
 }
