65e7f6f82e335f4e0af22ac418b79c89e999de64
Akin94 <akin.tutkan@gmail.com>
2023-01-10 15:45:22 +0300

    Add sign-in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react"
 import { signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils"
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup()
+       const {user} = await signInWithGooglePopup()
        const userDocRef = await createUserDocumentFromAuth(user)
+       const [form,setForm] = useState("") 
     }
     return (
-     <div>
-      <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-     </div>
+        <form //onSubmit={onSubmit} // submit olduğunda çalışsın 
+		> 
+			<div>
+				<input
+					name="email"
+                    type="email"
+					placeholder="Email"
+					value={form.fullname}
+					onChange={onChangeInput} // onchange olduğunda onChangeinput fonk çalışsın
+				/>
+			</div>
+
+			<div>
+				<input
+					name="password"
+                    type="password"
+					placeholder="Password"
+					value={form.password}
+					onChange={onChangeInput}
+				/>
+			</div>
+
+			<div className="btn">
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			</div>
+		</form>
+      
+     
     )
    }
-
    export default SignIn
