641d68ecacc0f9efdd060d00afd23ed6db8d20dd
Akin94 <akin.tutkan@gmail.com>
2023-01-25 12:58:00 +0300

    Add cart-item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 // import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../button/custom-button.component';
-// import CartItem from '../cart-item/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
 // import { selectCartItems } from '../../redux/cart/cart.selectors';
 // import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
@@ -12,7 +12,7 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
-    {/* <div className='cart-items'>
+    <div className='cart-items'>
       {cartItems.length ? (
         cartItems.map(cartItem => (
           <CartItem key={cartItem.id} item={cartItem} />
@@ -20,7 +20,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
       ) : (
         <span className='empty-message'>Your cart is empty</span>
       )}
-    </div> */}
+    </div>
     <CustomButton
       onClick={() => {
         history.push('/checkout');
