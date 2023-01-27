c55289fa2dea03c747f2fd905705900cbe9422b9
Akin94 <akin.tutkan@gmail.com>
2023-01-28 00:28:49 +0300

    get data from firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import {initializeApp} from "firebase/app"
 import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider,createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged} from "firebase/auth"
 
-import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
+import {getFirestore, doc, getDoc, setDoc,collection,writeBatch} from "firebase/firestore"
 import { Await } from "react-router-dom";
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -27,6 +27,17 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = ()=> signInWithRedirect(auth,googleProvider);
 
   export const db = getFirestore() 
+  export const addCollectionAndDocuments = async (collectionKey,objectsToAdd,field )=>{
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+    objectsToAdd.forEach((object) =>{
+      const docRef = doc(collectionRef, object.title.toLowerCase())
+      batch.set(docRef,object)
+    })
+    await batch.commit();
+    console.log("done")
+
+  }
 
 
   export const createUserDocumentFromAuth = async (userAuth,additionalInformation={}) => {
