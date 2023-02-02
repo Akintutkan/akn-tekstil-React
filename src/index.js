ec69de389072511a599150e2b97c55c1fdc7d41c
Akin94 <akin.tutkan@gmail.com>
2023-02-02 18:00:48 +0300

    add Redux Persist

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import {render} from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import "./index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import { PersistGate } from "redux-persist/integration/react";
+
+import App from "./App";
+import { store,persistor } from "./store/store";
+
+import "./index.scss";
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading ={null} persistor={persistor}>
     <Router>
         <App />
     </Router>
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,22 @@
 import { compose, configureStore, } from "@reduxjs/toolkit";
 import { applyMiddleware } from "@reduxjs/toolkit";
-// import  logger  from "redux-logger";
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import  logger  from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-    next(action)
+
+const persistConfig  = {
+    key: "root",
+    storage,
+    blacklist: ["user"]
 }
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares))
-export const store = configureStore(rootReducer,undefined,composedEnhancers)
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean);
+const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
+export const store = configureStore(persistedReducer,undefined,composedEnhancers)
+
+export const persistor = persistStore(store)
