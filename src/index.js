1981622cbaca3ecb829a6e6477a13ada8854233b
Akin94 <akin.tutkan@gmail.com>
2023-02-02 16:10:46 +0300

    Create cart redux

--- src/index.js
+++ src/index.js
@@ -6,19 +6,13 @@ import { store } from "./store/store";
 import "./index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
     <Router>
-  
-       
-          <CartProvider>
         <App />
-          </CartProvider>
-        
     </Router>
     </Provider>
   </React.StrictMode>
